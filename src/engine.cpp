841266b01ad1a3423af3203041c5bb270aae5d95
el-robo <eldad.roobol@chyronhego.com>
2019-02-20 16:52:50 +0100

    sample_rate as member, adding mod functions

--- src/engine.cpp
+++ src/engine.cpp
@@ -73,72 +73,112 @@ auto clip( float value )
 
 struct generator
 {
-	using function = std::function< double( generator &, double ) >;
+	using function = std::function< double( generator & ) >;
 	const function wave_function;
 
-	double frequency = 440.0;
+	double frequency_ = 440.0;
+	double amplitude_ = 0.1;
 	double t = 0.0;
-	double amplitude = 0.1;
+	double sample_rate_ = 480000.0;
 
-	double operator()( double sample_rate )
+	struct
+	{
+		std::function< double() > frequency;
+		std::function< double() > amplitude;
+	} mod;
+
+	double frequency()
+	{
+		if( mod.frequency )
+		{
+			return frequency_ * mod.frequency();
+		}
+
+		return frequency_;
+	}
+
+	double amplitude()
+	{
+		if( mod.amplitude )
+		{
+			return amplitude_ * mod.amplitude();
+		}
+
+		return amplitude_;
+	}
+
+	double operator()()
 	{
 		if( wave_function )
 		{
-			return wave_function( *this, sample_rate );
+			return wave_function( *this );
 		}
 
 		return 0.0;
 	}
 };
 
-void advance( generator &g, double period, double sample_rate )
+void advance( generator &g, double period )
 {
-	g.t += (period / sample_rate) * g.frequency;
+	g.t += (period / g.sample_rate_) * g.frequency();
 }
 
-float sine( generator &g, double sample_rate )
+float sine( generator &g )
 {
-	const auto value = g.amplitude * clip( sin( g.t ) );
-	advance( g, two_pi, sample_rate );
+	const auto value = g.amplitude() * clip( sin( g.t ) );
+	advance( g, two_pi );
 	return value;
 }
 
-float square( generator &g, double sample_rate )
+float square( generator &g )
 {
-	auto value = sine( g, sample_rate );
-	return g.amplitude * (value > 0.0 ? 1.0 : -1.0);
+	return g.amplitude() * (sine( g ) > 0.0 ? 1.0 : -1.0);
 }
 
-float saw( generator &g, double sample_rate )
+float saw( generator &g )
 {
-	const auto value = g.amplitude * (g.t - std::floor( g.t ));
-	advance( g, 1.0, sample_rate );
+	const auto value = g.amplitude() * (g.t - std::floor( g.t ));
+	advance( g, 1.0 );
 	return value;
 }
 
 using voice = std::vector< generator >;
 
 template < typename Buffer >
-void generate( Buffer &buffer, voice &v, double sample_rate )
+void generate( Buffer &buffer, voice &v )
 {
-	std::generate( buffer.begin(), buffer.end(), [ &v, sample_rate ]()
+	std::generate( buffer.begin(), buffer.end(), [ &v ]()
 	{
-		return std::accumulate( v.begin(), v.end(), 0.0, [ sample_rate ]( double value, auto &generator )
+		return std::accumulate( v.begin(), v.end(), 0.0, []( double value, auto &generator )
 		{
-			return value + generator( sample_rate );
+			return value + generator();
 		} );
 	} );
+}
+
+void set_sample_rate( std::vector< voice > &voices, double sample_rate )
+{
+	for( auto &voice : voices )
+	{
+		for( auto &gen : voice )
+		{
+			gen.sample_rate_ = sample_rate;
+		}
+	}
 }
 
 void run_engine( engine::implementation &impl )
 {
 	auto freq = 220.0;
+
 	std::vector< voice > voices
 	{
 		{ { sine, freq } },
 		{ { square, freq } }
 		// { { square } }
 	};
+
+	set_sample_rate( voices, impl.audio.sample_rate() );
 
 	while( impl.running )
 	{
@@ -150,7 +190,7 @@ void run_engine( engine::implementation &impl )
 
 		while( buffer != buffers.end() && voice != voices.end() )
 		{
-			generate( *buffer++, *voice++, impl.audio.sample_rate() );
+			generate( *buffer++, *voice++ );
 		}
 
 		frame.promised_buffers.set_value( std::move( buffers ) );
